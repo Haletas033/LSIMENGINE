9ba52811965a38c9fce3cac827dea5de429ca43f
Alex Curran <haletas88@gmail.com>
2026-02-26 18:00:01 +1300

    Add LSIMENGINE repo card styles

---
+++ Website/Src/styles/LSIMENGINE_Styles.css.h
@@ -0,0 +1,15 @@
+#ifndef LSIMENGINE_STYLES_CSS_H
+#define LSIMENGINE_STYLES_CSS_H
+
+#include "../style.h"
+
+//Currently a test
+inline void LSIMENGINE() {
+    Style LSIMENGINEid("#LSIMENGINE");
+    LSIMENGINEid
+        .put("color", "blue");
+
+    Builder::Build("LSIMENGINE_Styles");
+}
+
+#endif //STYLE_LSIMENGINE_STYLES_CSS_H
