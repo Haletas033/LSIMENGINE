0462236748d6a15545572a2efd52b0d6eead92ad
haletas <haletas033@gmail.com>
2026-08-11 20:50:21 +1200

    feat: add support for terrain generation using ECS

--- Src/geometry/mesh.cpp
+++ Src/geometry/mesh.cpp
@@ -1,7 +1,11 @@
 #include <geometry/mesh.h>
 
 #include "ECS/name.h"
+#include "geometry/terrain.h"
 #include "rendering/meshRenderer.h"
+
+extern Defaults engineDefaults;
+extern std::string workingDir;
 
 EntityHandle Mesh::create(const Primitive::Type primitive, const MeshMode mode, Registry &registry, MeshPool &meshPool, const Material &material, const Transform &transform) {
         MeshData meshData{};
@@ -22,8 +26,27 @@ EntityHandle Mesh::create(const Primitive::Type primitive, const MeshMode mode, 
                 case Primitive::TORUS:
                         meshData = Primitive::GenerateTorus(24, 12, 1.f, 0.2f,1.f);
                         break;
-                case Primitive::TERRAIN:
-                        break; // TODO
+                case Primitive::TERRAIN: {
+                        std::vector<std::vector<float>> noiseMap = Terrain::GenerateNoiseMap(engineDefaults.size, engineDefaults.size, static_cast<int>(time(nullptr)),
+                                engineDefaults.scale, engineDefaults.octaves, engineDefaults.persistence, engineDefaults.lacunarity);
+
+                        const auto uID = static_cast<long long>(glfwGetTime() * 1'000'000'000LL);
+                        const std::string outputPath = std::string(workingDir) + "resources/" + std::to_string(uID) + "terrain.png";
+                        const uint32_t noiseMapTexture = Terrain::noiseMapToTexture(noiseMap, outputPath.c_str());
+
+                        std::vector<float> vertices;
+                        std::vector<uint32_t> indices;
+
+                        Terrain::noiseMapToMesh(noiseMap, vertices, indices, engineDefaults.heightScale, engineDefaults.gridScale);
+
+                        const EntityHandle mesh = create(vertices, indices, mode, registry, meshPool, material, transform);
+                        registry.getComponent<Name>(mesh)->value = "Terrain";
+                        Material *mat = registry.getComponent<Material>(mesh);
+                        mat->setProperty("useTexture", 1);
+                        // newMesh->texturePath = std::to_string(uID) + "terrain.png";
+                        mat->addProperty<int>("texId", static_cast<int>(noiseMapTexture));
+                        return mesh;
+                }
                 case Primitive::MODEL:
                         break; // TODO
         }
